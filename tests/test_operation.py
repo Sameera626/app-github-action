aaa83b4b8d6877cf97f6993586115373e5f06f4b
Sameera Athukorala <151955778+Sameera626@users.noreply.github.com>
2025-07-09 12:18:02 +0530

    Update test_operation.py

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -6,7 +6,7 @@ def test_add():
     assert add(0, 0) == 0
 
 def test_sub():
-    assert sub(2, 1) == 10
+    assert sub(2, 1) == 1
     assert sub(1, 1) == 0
     assert sub(0, 1) == -1
     assert sub(-1, -1) == 0
